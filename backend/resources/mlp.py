aa56ffe0241971da9a7d2a83e0f7e7bc0862139c
Lucas Rocha <lr359508@gmail.com>
2024-02-08 14:11:49 -0300

    feat: IrisSchemas created

--- backend/resources/mlp.py
+++ backend/resources/mlp.py
@@ -1,13 +1,14 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint
 from joblib import load
+from schemas import IrisFeatures, IrisPredict
 
 bp = Blueprint("mlp", __name__, description="Operations on mlp")
 
 @bp.route("/iris")
 class Iris(MethodView):
-    @bp.arguments(TodoSchema)
-    @bp.response(200,TodoSchema(many=True))
+    @bp.arguments(IrisFeatures)
+    @bp.response(200,IrisPredict)
     def get(self, features):
         model = load('model.joblib')
         predict = model.predict(**features)
---
+++ backend/schemas/iris.py
@@ -0,0 +1,10 @@
+from marshmallow import Schema, fields
+
+class IrisFeatures(Schema):
+    Sepal_Length = fields.Float(required=True)
+    Sepal_Width = fields.Float(required=True)
+    Petal_Length = fields.Float(required=True)
+    Petal_Width = fields.Float(required=True)
+
+class IrisPredict(IrisFeatures):
+    Species = fields.Float()
